5d586b6605edfe52d7a6152a581d82236238ef72
Anton Leybov <leybov.anton@gmail.com>
2022-11-11 21:39:53 +0200

    fix: eol arrts;

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    'linebreak-style': ['error', 'unix'],
     'max-len': [2, { code: 80, ignorePattern: '^import .*' }],
     'indent': ['warn', 2],
     'object-curly-spacing': ['warn', 'always'],
